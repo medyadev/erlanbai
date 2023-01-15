84a2aa09f182b6f41a8bacd2eb989ac79cd42464
medyadev <kazyevmedya@gmail.com>
2023-01-15 14:26:04 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,84 +1,65 @@
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useEffect, useRef } from "react";
-import "./styles.css";
+import {useState} from 'react';
+import {useForm} from 'react-hook-form';
+import api from "./api";
+import './App.css'
+import {ToastContainer, toast} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
-gsap.registerPlugin(ScrollTrigger);
+// const useInput = (initialValue) => {
+//     const [value, setValue] = useState(initialValue)
+//     const [dirty, setDirty] = useState(false)
+//
+//     const onChange = (e) => {
+//         setValue(e.target.value)
+//     }
+//     const blur = (e) => {
+//         setDirty(true)
+//     }
+//     return {
+//         value,
+//         onChange,
+//         blur
+//     }
+//
+// }
 
-export default function App() {
-  const panels = useRef([]);
-  const panelsContainer = useRef();
 
-  const createPanelsRefs = (panel, index) => {
-    panels.current[index] = panel;
-  };
+// const toastify = (text) => {
+//     toast(text)
+// }
+const App = () => {
+    const {register, handleSubmit,reset, formState: {errors}} = useForm();
+    const [nameError, setNameError] = useState('емайл не может быть пустым')
+    const [passwordError, setPasswordError] = useState('')
+    const onSubmites = data => {
+        api.post(`contact/0101`, {
+            name: data.name,
+            number: data.number,
+        })
+            .then((red) => {
+                console.log(red.data)
+                toast.success("Success!")
+            })
+            .catch((err) =>{
+                toast.error('error')
+            })
+        reset()
+    }
 
-  useEffect(() => {
-    const totalPanels = panels.current.length;
-
-    gsap.to(panels.current, {
-      xPercent: -100 * (totalPanels - 1),
-      ease: "none",
-      scrollTrigger: {
-        trigger: panelsContainer.current,
-        pin: true,
-        scrub: 1,
-        snap: 1 / (totalPanels - 1),
-        // base vertical scrolling on how wide the container is so it feels more natural.
-        end: () => "+=" + panelsContainer.current.offsetWidth
-      }
-    });
-  }, []);
-
-  return (
-      <>
-        <div className="container" ref={panelsContainer}>
-          <div
-              className="description panel blue"
-              ref={(e) => createPanelsRefs(e, 0)}
-          >
-            <div>
-              <h1>Horizontal snapping sections (simple)</h1>
-              <p>
-                Scroll vertically to scrub the horizontal animation. It also
-                dynamically snaps to the sections in an organic way based on the
-                velocity. The snapping occurs based on the natural ending position
-                after momentum is applied, not a simplistic "wherever it is when
-                the user stops".
-              </p>
-              <div className="scroll-down">
-                Scroll down<div className="arrow"></div>
-              </div>
-            </div>
-          </div>
-          <section className="panel red" ref={(e) => createPanelsRefs(e, 1)}>
-            ONE
-          </section>
-          <section className="panel orange" ref={(e) => createPanelsRefs(e, 2)}>
-            TWO
-          </section>
-          <section className="panel purple" ref={(e) => createPanelsRefs(e, 3)}>
-            THREE
-          </section>
-          <section className="panel green" ref={(e) => createPanelsRefs(e, 4)}>
-            FOUR
-          </section>
-          <section className="panel gray" ref={(e) => createPanelsRefs(e, 5)}>
-            FIVE
-          </section>
+    return (
+        <div className='App'>
+            <form className='from' onSubmit={handleSubmit(onSubmites)}>
+                <ToastContainer/>
+                <h1>register</h1>
+                <input className='email' name='name' type="text"
+                       placeholder="Last name" {...register("name", {required: true})} />
+                {errors.name && <div style={{color: 'red'}}>name не может быть пустым</div>}
+                <input className='password' name='password' type="tel"
+                       placeholder="Mobile number" {...register("number", {required: true})} />
+                {errors.number && <div style={{color: "red"}}>номер не может быть пустым</div>}
+                <button className='btn' type="submit">бас мени ещак</button>
+            </form>
         </div>
-
-        <header>
-          <a href="https://greensock.com/scrolltrigger">
-            <img
-                className="greensock-icon"
-                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/scroll-trigger-logo-light.svg"
-                width="200"
-                height="64"
-                alt="GSAP"
-            />
-          </a>
-        </header>
-      </>
-  );
-}
+    );
+};
+export default App;
---
+++ src/api.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: `https://kachigosasdf.pythonanywhere.com/api/v1/`,
+})
+
+export default api;
